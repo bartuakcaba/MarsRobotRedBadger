25eb795f7c8a6ac9ad451ebb37e26593c161856a
Bartu <bartu@akcaba.com>
2018-02-03 17:50:55 +0000

    robot initialisation test passes

--- src/Robot.java
+++ src/Robot.java
@@ -6,4 +6,10 @@ public class Robot {
     public int x;
     public int y;
     public int dir;
+
+    public Robot(int x, int y, int dir) {
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+    }
 }
